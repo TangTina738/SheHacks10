aa240dbda8bdf9d1c3c9a6ec4e07d7d3e63e7d49
TangTina738 <Tangtina738@gmail.com>
2026-01-10 16:33:36 -0500

    Backend language app!

--- minilingo/src/main.jsx
+++ minilingo/src/main.jsx
@@ -1,17 +1,11 @@
-
 import "bootstrap/dist/css/bootstrap.min.css";
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Nav from './Nav.jsx'
-import Home from './Home.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Nav/>
-    <Home/>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
